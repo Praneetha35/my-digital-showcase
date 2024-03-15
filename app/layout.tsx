adaaf8a2192b1dd54015403fe39dde651f7be380
Praneetha <praneethacp3535@gmail.com>
2024-03-15 23:10:36 +0530

    Completely changed the design and theme

---
+++ app/layout.tsx
@@ -0,0 +1,68 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Praneetha",
+    template: "%s | Praneetha",
+  },
+  openGraph: {
+    title: "Praneetha",
+    siteName: "Praneetha's Portfolio",
+    images: [
+      {
+        url: "https://i.pinimg.com/474x/70/8d/f8/708df807416f14743bb08ffee72cd544.jpg",
+        width: 421,
+        height: 421,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    shortcut: "/female-dev.jpeg",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
