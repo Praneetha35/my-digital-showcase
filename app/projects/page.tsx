08337f5ddd0f13ce06676199a0418313a6a081e8
Praneetha35 <praneethacp3535@gmail.com>
2024-03-20 12:38:28 -0400

    Added project details

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,33 +10,33 @@ export default async function ProjectsPage() {
   const mockProjects = [
     {
       key: 1,
-      title: "Code Quest",
+      title: "BidVerse",
       description:
-        "Embark on a thrilling journey through the coding realm. Join our hero as they conquer challenges and unravel the mysteries of programming.",
+        "BidVerse is a Polygon based web3 platform revolutionizing hiring: seamlessly connecting companies with deserving candidates.",
       date: 1678742400000,
       views: 5,
     },
     {
       key: 2,
-      title: "Data Dance",
+      title: "SecureSign",
       description:
-        "Witness the enchanting dance of data elements. From graceful strings to rhythmic numbers, our JSON dancers twirl and spin, creating a symphony of structured information.",
+        "A web3 PDF uploader with IPFS storage and blockchain integration for cryptographic verification, ensuring secure file management.",
       date: 1678742400000,
       views: 8,
     },
     {
       key: 3,
-      title: "Pixel Poetry",
+      title: "PixelPython",
       description:
-        "Explore the artistry of pixels in this poetic journey. Each pixel tells a story, and together they create a masterpiece of digital expression.",
+        "PixelPython is a classic snake game using Pygame library.",
       date: 1678742400000,
       views: 3,
     },
     {
       key: 4,
-      title: "Byte Breeze",
+      title: "SkyWatch",
       description:
-        "Feel the refreshing byte breeze as you navigate the digital landscape. Let the bits and bytes guide you through a seamless and breezy coding experience.",
+        "SkyWatch is a dynamic React JS weather app showcasing temperature, region, and local time display.",
       date: 1678742400000,
       views: 7,
     },
@@ -47,46 +47,6 @@ export default async function ProjectsPage() {
         "Delve into the fascinating world of cryptocurrencies. Join our virtual explorer as they unveil the tales and adventures within the blockchain.",
       date: 1678742400000,
       views: 2,
-    },
-    {
-      key: 6,
-      title: "Bug Ballet",
-      description:
-        "Experience the elegant ballet of bugs. Watch as they pirouette through the code, creating a dance of debugging and choreographed fixes.",
-      date: 1678742400000,
-      views: 9,
-    },
-    {
-      key: 7,
-      title: "Infinite Loops",
-      description:
-        "Embark on a mesmerizing journey through infinite loops. Explore the beauty and challenges of cyclical patterns in the ever-expanding digital universe.",
-      date: 1678742400000,
-      views: 4,
-    },
-    {
-      key: 8,
-      title: "Pixel Pioneers",
-      description:
-        "Join the pioneers of pixel artistry. Discover the pixelated landscapes created by visionary artists pushing the boundaries of digital creativity.",
-      date: 1678742400000,
-      views: 6,
-    },
-    {
-      key: 9,
-      title: "Code Canvas",
-      description:
-        "Paint your code on the digital canvas. Let your creativity flow as you brush strokes of logic and syntax to create a masterpiece of programming.",
-      date: 1678742400000,
-      views: 1,
-    },
-    {
-      key: 10,
-      title: "Binary Beats",
-      description:
-        "Groove to the binary beats of the digital realm. Each 0 and 1 contributes to the rhythm, creating a symphony of electronic harmony.",
-      date: 1678742400000,
-      views: 10,
     },
   ];
   // const views = (
@@ -133,7 +93,7 @@ export default async function ProjectsPage() {
           <Card>
               <article className="relative w-full h-full p-4 md:p-8">
                 <div className="flex items-center justify-between gap-2">
-                  <div className="text-xs text-zinc-100">
+                  {/* <div className="text-xs text-zinc-100">
                     {featured.date ? (
                       <time dateTime={new Date(featured.date).toISOString()}>
                         {Intl.DateTimeFormat(undefined, {
@@ -143,13 +103,13 @@ export default async function ProjectsPage() {
                     ) : (
                       <span>SOON</span>
                     )}
-                  </div>
-                  <span className="flex items-center gap-1 text-xs text-zinc-500">
+                  </div> */}
+                  {/* <span className="flex items-center gap-1 text-xs text-zinc-500">
                     <Eye className="w-4 h-4" />{" "}
                     {Intl.NumberFormat("en-US", { notation: "compact" }).format(
                       featured.views
                     )}
-                  </span>
+                  </span> */}
                 </div>
 
                 <h2
@@ -161,11 +121,11 @@ export default async function ProjectsPage() {
                 <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
                   {featured.description}
                 </p>
-                <div className="absolute bottom-4 md:bottom-8">
+                {/* <div className="absolute bottom-4 md:bottom-8">
                   <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
                     Read more <span aria-hidden="true">&rarr;</span>
                   </p>
-                </div>
+                </div> */}
               </article>
           </Card>
 
