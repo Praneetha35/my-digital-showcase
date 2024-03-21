69d8073ce52a8770d670bc56ceda40a76f5f25e0
Praneetha <praneethacp3535@gmail.com>
2024-03-21 11:53:26 +0530

    Added tech stacks

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -1,37 +1,25 @@
-import Link from "next/link";
-import { Eye, View } from "lucide-react";
 
 type Props = {
-	project: { title: string; description: string; date?: number; views: number};
-	views: number;
+  project: { title: string; description: string; date?: number; views: number; stacks: string[] };
 };
 
-export const Article: React.FC<Props> = ({ project, views }) => {
-	return (
-			<article className="p-4 md:p-8">
-				<div className="flex justify-between gap-2 items-center">
-					{/* <span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
-						{project.date ? (
-							<time dateTime={new Date(project.date).toISOString()}>
-								{Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
-									new Date(project.date),
-								)}
-							</time>
-						) : (
-							<span>SOON</span>
-						)}
-					</span> */}
-					{/* <span className="text-zinc-500 text-xs  flex items-center gap-1">
-						<Eye className="w-4 h-4" />{" "}
-						{Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
-					</span> */}
-				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-					{project.title}
-				</h2>
-				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-					{project.description}
-				</p>
-			</article>
-	);
+export const Article: React.FC<Props> = ({ project }) => {
+  return (
+    <article className="p-4 md:p-8">
+      <div className="flex justify-between gap-2 items-center"></div>
+      <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+        {project.title}
+      </h2>
+      <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+        {project.description}
+      </p>
+      <div className="mt-4">
+        {project.stacks.map((el) => (
+          <div className="bg-gray-800 p-2 mr-4 mb-4 rounded-lg shadow-md inline-block max-w-full">
+            <p className="text-gray-400 text-sm">{el}</p>
+          </div>
+        ))}
+      </div>
+    </article>
+  );
 };
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 import { Article } from "./article";
-import { Eye } from "lucide-react";
 
 export const revalidate = 60;
 export default async function ProjectsPage() {
-
-  const mockProjects = [
+  const projects = [
     {
       key: 1,
       title: "Market Research Analysis Tool",
@@ -15,6 +13,7 @@ export default async function ProjectsPage() {
         "Contributed to a Market Research Analysis tool streamlines federal acquisition decision-making by leveraging technology for market research, vendor identification, and small business goal fulfillment",
       date: 1678742400000,
       views: 5,
+      stacks: ["React.js", "Node.js", "MongoDB", "Angular"],
     },
     {
       key: 2,
@@ -23,6 +22,7 @@ export default async function ProjectsPage() {
         "Contributed to building an NFT Marketplace right from scratch, focusing on full-stack development, smart contract creation, and integrations like automated notifications via Mailgun, enhancing user experience and reducing missed auction events",
       date: 1678742400000,
       views: 5,
+      stacks: ["Vue.js", "Express.js", "PostgreSQL"],
     },
     {
       key: 3,
@@ -31,6 +31,7 @@ export default async function ProjectsPage() {
         "BidVerse is a Polygon based web3 platform revolutionizing hiring: seamlessly connecting companies with deserving candidates",
       date: 1678742400000,
       views: 5,
+      stacks: ["Node.js", "MongoDB", "Angular", "PostgreSQL"],
     },
     {
       key: 4,
@@ -39,22 +40,24 @@ export default async function ProjectsPage() {
         "I crafted a digital showcase - my portfolio website - to flaunt my projects and skills, because let's face it, my work deserves its own stage!",
       date: 1678742400000,
       views: 5,
+      stacks: ["Node.js", "MongoDB", "Angular", "PostgreSQL"],
     },
     {
-      key:5,
+      key: 5,
       title: "SecureSign",
       description:
         "A web3 PDF uploader with IPFS storage and blockchain integration for cryptographic verification, ensuring secure file management",
       date: 1678742400000,
       views: 8,
+      stacks: ["Node.js", "MongoDB", "Angular", "PostgreSQL"],
     },
     {
       key: 6,
       title: "PixelPython",
-      description:
-        "PixelPython is a classic snake game using Pygame library",
+      description: "PixelPython is a classic snake game using Pygame library",
       date: 1678742400000,
       views: 3,
+      stacks: ["Node.js", "MongoDB", "Angular", "PostgreSQL"],
     },
     {
       key: 7,
@@ -63,33 +66,9 @@ export default async function ProjectsPage() {
         "SkyWatch is a dynamic React JS weather app showcasing temperature, region, and local time display",
       date: 1678742400000,
       views: 7,
-    }
+      stacks: ["Node.js", "MongoDB", "Angular", "PostgreSQL"],
+    },
   ];
-  // const views = (
-  //   await redis.mget<number[]>(
-  //     ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":"))
-  //   )
-  // ).reduce((acc, v, i) => {
-  //   acc[allProjects[i].slug] = v ?? 0;
-  //   return acc;
-  // }, {} as Record<string, number>);
-
-  const featured = mockProjects.find((project) => project.key === 1)!;
-  const top2 = mockProjects.find((project) => project.key === 2)!;
-  const top3 = mockProjects.find((project) => project.key === 3)!;
-  const sorted = mockProjects
-    // .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.key !== featured.key &&
-        project.key !== top2.key &&
-        project.key !== top3.key
-    )
-    .sort(
-      (a, b) =>
-        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
-    );
 
   return (
     <div className="relative pb-16">
@@ -103,83 +82,31 @@ export default async function ProjectsPage() {
             Some of the projects are from work and some are on my own time.
           </p>
         </div>
-        <div className="w-full h-px bg-zinc-800" />
-
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          <Card>
-              <article className="relative w-full h-full p-4 md:p-8">
-                <div className="flex items-center justify-between gap-2">
-                  {/* <div className="text-xs text-zinc-100">
-                    {featured.date ? (
-                      <time dateTime={new Date(featured.date).toISOString()}>
-                        {Intl.DateTimeFormat(undefined, {
-                          dateStyle: "medium",
-                        }).format(new Date(featured.date))}
-                      </time>
-                    ) : (
-                      <span>SOON</span>
-                    )}
-                  </div> */}
-                  {/* <span className="flex items-center gap-1 text-xs text-zinc-500">
-                    <Eye className="w-4 h-4" />{" "}
-                    {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                      featured.views
-                    )}
-                  </span> */}
-                </div>
-
-                <h2
-                  id="featured-post"
-                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
-                >
-                  {featured.title}
-                </h2>
-                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                  {featured.description}
-                </p>
-                {/* <div className="absolute bottom-4 md:bottom-8">
-                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                    Read more <span aria-hidden="true">&rarr;</span>
-                  </p>
-                </div> */}
-              </article>
-          </Card>
-
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {[top2, top3].map((project, k) => (
-              <Card key={k}>
-                <Article project={project} views={project.views} />
-              </Card>
-            ))}
-          </div>
-        </div>
-        <div className="hidden w-full h-px md:block bg-zinc-800" />
-
         <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
           <div className="grid grid-cols-1 gap-4">
-            {sorted
+            {projects
               .filter((_, i) => i % 3 === 0)
               .map((project) => (
                 <Card key={project.key}>
-                  <Article project={project} views={project.views} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
           <div className="grid grid-cols-1 gap-4">
-            {sorted
+            {projects
               .filter((_, i) => i % 3 === 1)
               .map((project) => (
                 <Card key={project.key}>
-                  <Article project={project} views={project.views} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
           <div className="grid grid-cols-1 gap-4">
-            {sorted
+            {projects
               .filter((_, i) => i % 3 === 2)
               .map((project) => (
                 <Card key={project.key}>
-                  <Article project={project} views={project.views} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
