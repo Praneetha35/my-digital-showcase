f830f7ce49708e13f600ca49b7c38b385b9eafd0
Praneetha <praneethacp35@gmail.com>
2024-07-25 20:01:00 +0530

    Fixed the image and build issues

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -1,15 +1,33 @@
-
 type Props = {
-  project: { title: string; description: string; date?: number; views: number; stacks: string[],image?: string,githubUrl?:string};
+  project: {
+    title: string;
+    description: string;
+    date?: number;
+    views: number;
+    stacks: string[];
+    image?: string;
+    githubUrl?: string;
+  };
 };
 
 export const Article: React.FC<Props> = ({ project }) => {
   return (
     <article className="p-4 md:p-8">
       <div className="flex justify-between gap-2 items-center"></div>
-      <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-        {project.title}
-      </h2>
+      <div className="flex justify-between items-center">
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+          {project.title}
+        </h2>
+        {project.githubUrl && (
+          <a href={project.githubUrl} target="_blank">
+            <img
+              src="./github.png"
+              alt="Project"
+              className="w-[40px] h-[40px]"
+            />
+          </a>
+        )}
+      </div>
       <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
         {project.description}
       </p>
@@ -27,17 +45,6 @@ export const Article: React.FC<Props> = ({ project }) => {
           className="w-full h-auto object-cover rounded-lg mt-4"
         />
       )}
-      <div className="mt-4">
-        {project.githubUrl && (
-          <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
-            <img
-              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-              alt="GitHub"
-              className="w-8 h-8 inline-block"
-            />
-          </a>
-        )}
-      </div>
     </article>
   );
 };
